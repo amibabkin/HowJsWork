5a74d7125e895d6e184ae2d3d99b667a84d26af2
Andrey <amibabkin@gmail.com>
2024-03-27 21:46:08 +0700

    style: delete ;

--- ObjectGlobal/index.js
+++ ObjectGlobal/index.js
@@ -1,24 +1,24 @@
-'use strict'
+"use strict"
 
 const user = {
-    name: 'John',
-    age: 20
+  name: "John",
+  age: 20,
 }
 
-Object.defineProperty(user, 'name', {
-    writable: false
+Object.defineProperty(user, "name", {
+  writable: false,
 })
 
-const descriptor = Object.getOwnPropertyDescriptor(user, 'name')
+const descriptor = Object.getOwnPropertyDescriptor(user, "name")
 
 // user.name = 'Pete' // error
 
 console.log(descriptor)
 
-Object.defineProperty(user, 'age', {
-    enumerable: false
+Object.defineProperty(user, "age", {
+  enumerable: false,
 })
 
 for (let key in user) {
-    console.log(user[key])
+  console.log(user[key])
 }
